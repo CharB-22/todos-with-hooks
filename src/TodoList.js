b36fd6d73ea49aa6bf6427bce57e5df19a6e0c88
Charlene <c.bennevault@gmail.com>
2020-09-02 17:29:36 +0200

    Create TodoList with MaterialUi

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,24 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Divider from "@material-ui/core/Divider";
 
 export default function TodoList (props) {
 
     return(
-        <ul>
-            {props.todos.map((todo) => <li key={todo.id}>{todo.task}</li>)}
-        </ul>
-
+        <Paper>
+            <List>
+                {props.todos.map((todo) => 
+                <>
+                <ListItem key={todo.id}>
+                    <ListItemText>{todo.task}</ListItemText>
+                </ListItem>
+                <Divider/>
+                </>
+                )}
+            </List>
+        </Paper>
     )
 }
